aa5e411285c1802213c37404f0b6ee76b676ca46
Claudio Geronimo <claudiogeronimo@gmail.com>
2022-04-09 18:54:35 -0300

    Ancular course initla commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
 
 @Component({
@@ -10,13 +11,23 @@ import { Course } from './model/course';
 export class AppComponent {
 
   courses= COURSES;
+  course: any;
   
+  /* @ViewChild('cardRef1') //o puede usarse el nombre deo componente CourseCardComponent
+  card1: CourseCardComponent */
+
+  /* @ViewChild('cardRef2') //o puede usarse el nombre deo componente CourseCardComponent
+  card2: CourseCardComponent */
+
+  @ViewChild('container')
+  containerDiv: ElementRef;
+
     onCardClicked(){
     console.log("App Component - click on card ....");
   }
 
   onCourseSelected(course:Course){
-    console.log("App Component - click event bubbled ....",course);
+    console.log("container....", this.containerDiv);
   }
 
 }
